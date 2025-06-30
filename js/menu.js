7ff8e0ced73ab2a43d4f27bad97693a9f87004d6
Marcelo-115 <108548027+Marcelo-115@users.noreply.github.com>
2025-06-30 09:55:46 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,28 @@
-let btnMenu = document.getElementById('btn-menu');
-let menu = document.getElementById('menu-mobile');
-let overlay = document.getElementById('overlay-menu');
+// Obtém os elementos do menu mobile e overlay
+const btnMenu = document.getElementById('btn-menu');
+const menu = document.getElementById('menu-mobile');
+const overlay = document.getElementById('overlay-menu');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
+// Abre o menu mobile ao clicar no botão de abrir
+if (btnMenu && menu) {
+    btnMenu.addEventListener('click', () => {
+        menu.classList.add('abrir-menu');
+    });
+}
 
+// Fecha o menu ao clicar em qualquer lugar do menu mobile
+if (menu) {
+    menu.addEventListener('click', (event) => {
+        // Para evitar fechar o menu se clicar dentro do conteúdo do menu
+        if (event.target.closest('nav') === null) {
+            menu.classList.remove('abrir-menu');
+        }
+    });
+}
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-
-})
+// Fecha o menu ao clicar no overlay
+if (overlay) {
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('abrir-menu');
+    });
+}
