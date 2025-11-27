05236a8a3ddefec4b9aff4435e4f9aac567d5931
Marcelo-115 <108548027+Marcelo-115@users.noreply.github.com>
2025-11-27 13:16:59 -0300

    Delete js directory

--- js/rolagem-menu.js
+++
@@ -1,8 +0,0 @@
-// Altera a classe do cabeçalho com base na posição da rolagem
-window.addEventListener("scroll", () => {
-    const header = document.querySelector("header");
-    if (!header) return; // segurança: só continua se encontrar o <header>
-
-    // Aplica ou remove a classe "rolagem" se passou de 100px de rolagem
-    header.classList.toggle("rolagem", window.scrollY > 100);
-});
