c9ec7e66bbeac3c7497c5e3c812866012108daee
Marcelo-115 <108548027+Marcelo-115@users.noreply.github.com>
2025-12-03 18:36:59 -0300

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,8 @@
 export function loadFooter() {
-    fetch('/src/components/Footer/footer.html')
+    fetch('/src/components/footer/footer.html')
         .then(res => res.text())
         .then(html => {
             document.querySelector('footer').innerHTML = html;
         });
+
 }
